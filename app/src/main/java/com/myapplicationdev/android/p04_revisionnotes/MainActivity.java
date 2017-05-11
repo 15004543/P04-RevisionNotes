b9a6726ff1ce6af68da363f0865349c71394026f
Marcus Chong You Long <mugurox@gmail.com>
2017-05-12 00:19:44 +0800

    Marcus

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,14 +1,57 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNote;
+    Button btnInsert, btnShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNote = (EditText)findViewById(R.id.editTextNote);
+        btnInsert = (Button)findViewById(R.id.buttonInsertNote);
+        btnShow = (Button)findViewById(R.id.buttonShowList);
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroupStars);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+                String strAmountStars = rb.getText() + "";
+                int amountStars = Integer.parseInt(strAmountStars);
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertNote(etNote.getText().toString(), amountStars);
+                db.close();
+            }
+        });
+        btnShow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,
+                        SecondActivity.class);
+                // Start the new activity
+                startActivity(i);
+            }
+        });
 
     }
 }
